2d0377bc8302cb0a0da228de6c19decd4a2c8471
Sophiedev872 <lisbethsofia65@gmail.com>
2024-07-12 03:45:33 -0400

    back

---
+++ backend/database/migrations/typeuser.php
@@ -0,0 +1,17 @@
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void {
+        Schema::create('typeuser', function (Blueprint $table) {
+            $table->id();
+            $table->string('type_name');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void {
+        Schema::dropIfExists('typeuser');
+    }
+};
---
+++ backend/database/migrations/users.php
@@ -0,0 +1,21 @@
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->unsignedTinyInteger('id_typeuser');
+            $table->string('telefono');
+            $table->string('correo');
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void {
+        Schema::dropIfExists('users');
+    }
+};
