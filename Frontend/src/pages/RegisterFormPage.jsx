1244754a66b38a77267ca88cde54a327c5607215
tadeoressio <tadeo.ressio@gmail.com>
2024-07-12 12:11:06 -0300

    added numeral prefix on registerForm

--- Frontend/src/pages/RegisterFormPage.jsx
+++ Frontend/src/pages/RegisterFormPage.jsx
@@ -7,81 +7,93 @@ import LogoGoogle from '../assets/LogoGoogle.svg';
 import LogoFacebook from '../assets/LogoFacebook.svg';
 import InputText from '../components/input/InputText';
 import InputPass from '../components/input/InputPass';
+import InputNumber from '../components/input/InputNumber';
 
 const RegisterFormPage = () => {
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [password, setPassword] = useState('');
+  const [password2, setPassword2] = useState('');
+  const [email, setEmail] = useState('');
+  const [number, setNumber] = useState('');
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const handleNameChange = (value) => {
+    setName(value);
+    console.log('Nombre actualizado:', value);
+  };
 
-    const handleNameChange = (value) => {
-        setEmail(value);
-        console.log('Nombre actualizado:', value);
-    };
+  const handleLastNameChange = (value) => {
+    setLastName(value);
+    console.log('Apellido actualizado:', value);
+  };    
 
-    const handleLastNameChange = (value) => {
-        setEmail(value);
-        console.log('Apellido actualizado:', value);
-    };
-    
-    const handlePasswordChange = (value) => {
-        setPassword(value);
-        console.log('Password actualizado:', value);
-    };
+  const handlePasswordChange = (value) => {
+    setPassword(value);
+    console.log('Password actualizado:', value);
+  };
 
-    const handlePassword2Change = (value) => {
-        setPassword(value);
-        console.log('Password2 actualizado:', value);
-    };
+  const handlePassword2Change = (value) => {
+    setPassword2(value);
+    console.log('Password2 actualizado:', value);
+  };
 
-    const handleEmailChange = (value) => {
-        setPassword(value);
-        console.log('Email actualizado:', value);
-    };
+  const handleEmailChange = (value) => {
+    setEmail(value);
+    console.log('Email actualizado:', value);
+  };
 
-    const handleNumberChange = (value) => {
-        setPassword(value);
-        console.log('Número actualizado:', value);
-    };
+  const handlePhoneChange = (isValid, value, countryData) => {
+    setNumber(value);
+    console.log('Número:', value);
+    console.log('Es válido:', isValid);
+    console.log('Datos del país:', countryData);
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = {
+      name,
+      lastName,
+      password,
+      password2,
+      email,
+      number,
     };
+    console.log('Datos del formulario:', formData);
+  };
 
   return (
     <form className="frm-1" onSubmit={handleSubmit}>
-        <Link id="linkID" className="link" to="/login">
-            ¿Ya tienes cuenta? Ingresa aquí
-        </Link>
-        <div id="div-but">
-            <ButtonGrey to="/google" img={LogoGoogle} text="Continuar con Google" />
-            <ButtonGrey to="/facebook" img={LogoFacebook} text="Continuar con Facebook" />
-        </div>
-        <div id="div-name">
-            <InputText type="text" placeholder="Nombre" onChange={handleNameChange} />
-            <InputText type="text" placeholder="Apellido" onChange={handleLastNameChange} />
-        </div>
-        <div id="div-pass">
-            <InputPass id="inp-pass" placeholder="Contraseña" onChange={handlePasswordChange}></InputPass>
-            <InputPass id="inp-pass" placeholder="Repita contraseña" onChange={handlePassword2Change}></InputPass>
-        </div>
-        <div id="div-input1">
-            <InputText id="mail"type="email" placeholder="Email" onChange={handleEmailChange} />
-            
-        </div>
-        <div id="div-input2">
-            <InputText type="email" placeholder="Teléfono" onChange={handleNumberChange} />
-        </div>
-        <div className="div-log">
-            <label>
-                <input type="checkbox" />
-                <span className="checkmark">Recuérdame</span>
-            </label>
-            <ButtonBg type="submit" text="Crear cuenta" />
-        </div>
+      <Link id="linkID" className="link" to="/login">
+        ¿Ya tienes cuenta? Ingresa aquí
+      </Link>
+      <div id="div-but">
+        <ButtonGrey to="/google" img={LogoGoogle} text="Continuar con Google" />
+        <ButtonGrey to="/facebook" img={LogoFacebook} text="Continuar con Facebook" />
+      </div>
+      <div id="div-name">
+        <InputText type="text" placeholder="Nombre" onChange={handleNameChange} />
+        <InputText type="text" placeholder="Apellido" onChange={handleLastNameChange} />
+      </div>
+      <div id="div-pass">
+        <InputPass id="inp-pass" placeholder="Contraseña" onChange={handlePasswordChange} />
+        <InputPass id="inp-pass" placeholder="Repita contraseña" onChange={handlePassword2Change} />
+      </div>
+      <div id="div-input1">
+        <InputText id="mail" type="email" placeholder="Email" onChange={handleEmailChange} />
+      </div>
+      <div id="div-input2">
+        <InputNumber placeholder="Teléfono" onPhoneNumberChange={handlePhoneChange} />
+      </div>
+      <div className="div-log">
+        <label>
+          <input type="checkbox" />
+          <span className="checkmark">Recuérdame</span>
+        </label>
+        <ButtonBg type="submit" text="Crear cuenta" />
+      </div>
     </form>
   );
 };
 
-export default RegisterFormPage
-
-// se debería restructurar con menos divs. Habría que juntar todas las funciones en una sola
+export default RegisterFormPage;
