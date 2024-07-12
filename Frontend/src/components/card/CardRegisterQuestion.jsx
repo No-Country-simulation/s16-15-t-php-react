d5d974af1234ba303cd3f64057327b2e31d2868a
tadeoressio <tadeo.ressio@gmail.com>
2024-07-12 14:09:31 -0300

    added choosed opt as var in FirstQuestionRegisterPage

--- Frontend/src/components/card/CardRegisterQuestion.jsx
+++ Frontend/src/components/card/CardRegisterQuestion.jsx
@@ -3,30 +3,39 @@ import '../../styles/cardRegisterQuestion.css';
 import registerCard1 from '../../assets/registerCard1.svg';
 import registerCard2 from '../../assets/registerCard2.svg';
 
-const CardRegisterQuestion = () => {
+const CardRegisterQuestion = ({ onOptionChange }) => {
+    
   const [selectedOption, setSelectedOption] = useState('');
 
   const handleOptionChange = (option) => {
     setSelectedOption(option);
-    console.log(option);
+    onOptionChange(option);
   };
 
   return (
     <div id="div-card">
-        <div id="div-card1" onClick={() => handleOptionChange('client')} className={selectedOption === 'client' ? 'selected' : ''}>
-            <span>Soy cliente en busca de talento <br/>freelance</span>
-            <img src={registerCard1} alt="Client" />
-            <div className="div-radio">
-                <input type="radio" checked={selectedOption === 'client'} onChange={() => handleOptionChange('client')}/>
-            </div>
+      <div id="div-card1" onClick={() => handleOptionChange('client')} className={selectedOption === 'client' ? 'selected' : ''}>
+        <span>Soy cliente en busca de talento <br />freelance</span>
+        <img src={registerCard1} alt="Client" />
+        <div className="div-radio">
+          <input
+            type="radio"
+            checked={selectedOption === 'client'}
+            onChange={() => handleOptionChange('client')}
+          />
         </div>
-        <div id="div-card2" onClick={() => handleOptionChange('freelancer')} className={selectedOption === 'freelancer' ? 'selected' : ''}>
-            <span>Soy freelancer <br/>en busca de proyectos</span>
-            <img src={registerCard2} alt="Freelancer" />
-            <div className="div-radio">
-                <input type="radio" checked={selectedOption === 'freelancer'} onChange={() => handleOptionChange('freelancer')}/>
-            </div>
-        </div>        
+      </div>
+      <div id="div-card2" onClick={() => handleOptionChange('freelancer')} className={selectedOption === 'freelancer' ? 'selected' : ''}>
+        <span>Soy freelancer <br />en busca de proyectos</span>
+        <img src={registerCard2} alt="Freelancer" />
+        <div className="div-radio">
+          <input
+            type="radio"
+            checked={selectedOption === 'freelancer'}
+            onChange={() => handleOptionChange('freelancer')}
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- Frontend/src/pages/FirstQuestionRegisterPage.jsx
+++ Frontend/src/pages/FirstQuestionRegisterPage.jsx
@@ -5,23 +5,30 @@ import CardRegisterQuestion from '../components/card/CardRegisterQuestion';
 import ButtonBg from '../components/button/ButtonBg';
 
 const FirstQuestionRegisterPage = () => {
+    
+  const [selectedOption, setSelectedOption] = useState('');
+
+  const handleOptionChange = (option) => {
+    setSelectedOption(option);
+    console.log(option);
+  };
 
   return (
     <main id="tag-main">
-        <div id="div-title">
-            <h2>¿Eres cliente o freelancer?</h2>
-        </div>
-        <div id="div-cards">
-            <CardRegisterQuestion></CardRegisterQuestion>
-        </div>
-        <div id="div-but-log">
-            <ButtonBg to="/registerForm" text="Crear cuenta"></ButtonBg>
-        </div>
-        <Link id="linkID" className="link" to="/login">
-            ¿Ya tienes cuenta? Ingresa aquí
-        </Link>
+      <div id="div-title">
+        <h2>¿Eres cliente o freelancer?</h2>
+      </div>
+      <div id="div-cards">
+        <CardRegisterQuestion onOptionChange={handleOptionChange} />
+      </div>
+      <div id="div-but-log">
+        <ButtonBg to="/registerForm" text="Crear cuenta" />
+      </div>
+      <Link id="linkID" className="link" to="/login">
+        ¿Ya tienes cuenta? Ingresa aquí
+      </Link>
     </main>
-  )
-}
+  );
+};
 
-export default FirstQuestionRegisterPage
+export default FirstQuestionRegisterPage;
