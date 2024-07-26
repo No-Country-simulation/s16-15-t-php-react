f00f5238df6d3178684bfc0ac673bb6467231c17
celinagrcs <celinagrcs@users.noreply.github.com>
2024-07-26 20:02:17 -0300

    create offersPage.module.css and add new button to offersPage

--- Frontend/src/pages/OffersPage.jsx
+++ Frontend/src/pages/OffersPage.jsx
@@ -1,5 +1,6 @@
+import ButtonBg from '../components/button/ButtonBg'
 import OfferProposalCard from '../components/card/OfferProposalCard'
-import styles from '../styles/proposalsPage.module.css'
+import styles from '../styles/offersPage.module.css'
 
 const OffersPage = () => {
   return (
@@ -8,7 +9,10 @@ const OffersPage = () => {
       <h2 className={styles.title}>Mis Ofertas</h2>
     </section>
     <section className={styles.mainContainer}>
-      <article className={styles.infoCard}>
+      <article className={styles.infoCardOffer}>
+        <div className={styles.buttonCreateOffer}>
+          <ButtonBg text="Crear oferta" />
+        </div>
         <p className={styles.infoTitle}>Actividad</p>
         <div className={styles.info}>
           <p className={styles.infoText}>Vistas actuales</p>
@@ -22,19 +26,19 @@ const OffersPage = () => {
         <p className={styles.infoTitle}>Proyectos actuales</p>
         <p className={styles.infoText}>Cuentas con 5 proyectos actualmente</p>
       </article>
-      <section className={styles.proposalsContainer}>
+      <section className={styles.offersContainer}>
         <OfferProposalCard 
-            title="Se busca diseñador web, asistente virtual, especialista en marketing en redes sociales y creador de logotipos para agencia creativa"
+            title="Editora de videos"
             countryandprice="País: Uruguay  -  $300USD por semana"
             text="Buscamos un entusiasta de TikTok con un don para crear contenido atractivo y viral. Si tienes un historial comprobado de creación de contenido, crecimiento orgánico de cuentas de TikTok y participación de audiencias, ¡trabajemos juntos!"
         />
         <OfferProposalCard
-            title="Creador de contenido para redes sociales"
+            title="Guionista para videos y contenido para redes sociales"
             countryandprice="País: Mexico  -  $300USD por semana"
             text="Buscamos un entusiasta de TikTok con un don para crear contenido atractivo y viral. Si tienes un historial comprobado de creación de contenido, crecimiento orgánico de cuentas de TikTok y participación de audiencias, ¡trabajemos juntos!"
         />
         <OfferProposalCard 
-            title="Creador de contenido de TikTok y experto en crecimiento y participación"
+            title="Redactora para posts de redes sociales en español/inglés"
             countryandprice="País: Colombia  -  $300USD por semana"
             text="Buscamos un entusiasta de TikTok con un don para crear contenido atractivo y viral. Si tienes un historial comprobado de creación de contenido, crecimiento orgánico de cuentas de TikTok y participación de audiencias, ¡trabajemos juntos!"
         />
